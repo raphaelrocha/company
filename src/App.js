022a9436ec998e63889b4468ad0b7655a0d280d8
raphael <raphaelrocha86@gmail.com>
2019-03-20 17:39:18 -0400

    estruturando o projeto.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Portfolio from "./components/Portfolio";
+import Pricing from "./components/Pricing";
+import Contact from "./components/Contact";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header/>
+          <Route path='/' exact component={Home}/>
+          <Route path='/services' component={Services}/>
+          <Route path='/portfolio' component={Portfolio}/>
+          <Route path='/pricing' component={Pricing}/>
+          <Route path='/contact' component={Contact}/>
+          <Footer/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Site.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Site = props => {
+  return (
+    <div>
+
+
+
+
+
+      {/* Image of location/map */}
+      <img src='/w3images/map.jpg' className='w3-image w3-greyscale-min'/>
+
+    </div>
+  )
+};
+
+export default Site;
