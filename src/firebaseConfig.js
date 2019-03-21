b051c795d92ef58df0b16968e4b4e67eec9d6376
raphael <raphaelrocha86@gmail.com>
2019-03-21 12:46:36 -0400

    configuração inicial do firebase.

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+const rebase = require('re-base');
+const firebase = require('firebase');
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyA4qdlTY2Ctmh2ERVBlyRKhMxfZol2wqqc",
+  authDomain: "company-87a0a.firebaseapp.com",
+  databaseURL: "https://company-87a0a.firebaseio.com",
+  projectId: "company-87a0a",
+  storageBucket: "company-87a0a.appspot.com",
+  messagingSenderId: "831159791709"
+};
+const app = firebase.initializeApp(config);
+const firebaseConfig = rebase.createClass(app.database());
+
+export default firebaseConfig;
