d8ed3116f7934f95691522f6eeb34f86db21d9f7
raphael <raphaelrocha86@gmail.com>
2019-03-21 17:35:19 -0400

    continua com o form para add portifolio

--- src/components/admin/screens/AdminPortfolio.js
+++ src/components/admin/screens/AdminPortfolio.js
@@ -9,21 +9,33 @@ export default class AdminPortfolio extends Component{
 
   }
 
+  savePortfolio = (e) => {
+    console.log('savePortfolio');
+    console.log(this.refs.title.value);
+    console.log(this.refs.description.value);
+    console.log(this.refs.image.value);
+
+    /*
+    Evita o reload automático da tela.
+     */
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div style={{padding:'120px'}}>
-        <form>
+        <form onSubmit={this.savePortfolio.bind(this)}>
           <div className="form-group">
             <label htmlFor="title">Título</label>
-            <input type="text" className="form-control" id="title" placeholder="Título"/>
+            <input type="text" className="form-control" id="title" placeholder="Título" ref={'title'}/>
           </div>
           <div className="form-group">
             <label htmlFor="description">Descriçao</label>
-            <textarea type="text" className="form-control" id="description" placeholder="Descrição"/>
+            <textarea type="text" className="form-control" id="description" placeholder="Descrição" ref={'description'}/>
           </div>
           <div className="form-group">
             <label htmlFor="image">Imagem</label>
-            <input type="file" className="form-control-file" id="image"/>
+            <input type="file" className="form-control-file" id="image" ref={'image'}/>
           </div>
           <button type="submit" className="btn btn-primary">Salvar</button>
         </form>
