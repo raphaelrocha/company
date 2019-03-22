e6498b10ba8219707b26b70e4d15cac3b410ebf7
raphael <raphaelrocha86@gmail.com>
2019-03-22 09:55:14 -0400

    protege o admin com login e senha.

--- src/components/admin/screens/Admin.js
+++ src/components/admin/screens/Admin.js
@@ -1,15 +1,42 @@
 import React, {Component} from 'react';
 import AdminMenu from "../components/AdminMenu";
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import AdminPortfolio from "./AdminPortfolio";
+import {firebaseAuth} from '../../../firebaseConfig'
 
 export default class Admin extends Component{
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      logged: false,
+      loginInProgress: true,
+      user: null
+    }
+  }
+
+  componentDidMount(){
+    firebaseAuth.onAuthStateChanged(user=>{
+      this.setState(
+        {
+          user,
+          loginInProgress: false,
+          logged: !!user
+        }
+      );
+    });
   }
 
   render() {
+    if(this.state.loginInProgress){
+      return (<h1>Aguarde....</h1>);
+    }
+
+    if(!this.state.logged){
+      return (<Redirect to={'/login'}/>);
+    }
+
     let url = this.props.match.url;
     return (
       <div>
